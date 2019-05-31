26d049365eadb6e09102decc1ca87dd569c75b10
livia_welschinger <livia-welschinger@t-online.de>
2019-05-31 16:54:53 +0200

    Registered the service as an Eureka client

--- src/main/java/de/livia/addierer/AddiererApplication.java
+++ src/main/java/de/livia/addierer/AddiererApplication.java
@@ -1,16 +1,35 @@
 package de.livia.addierer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Application class
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AddiererApplication {
 
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
 	public static void main(String[] args) {
 		SpringApplication.run(AddiererApplication.class, args);
+	}
+
+
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+			@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
 	}
 
 }
